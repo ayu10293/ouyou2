da2446a48a62604c3f3adf6a6c1b25a0fac733d6
EC2 Default User <ec2-user@ip-172-31-33-245.ap-northeast-1.compute.internal>
2023-09-06 00:24:42 +0000

    [finish]コメント機能

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,6 +1,6 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book.id])
+    book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
@@ -14,7 +14,7 @@ class BookCommentsController < ApplicationController
 
   private
   def book_comment_params
-    params.require(book_comment).permit(:comment)
+    params.require(:book_comment).permit(:comment)
   end
 
 end
