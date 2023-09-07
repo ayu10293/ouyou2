48e910dbe4f524d79f8dcbd9b62d42b35fe578b4
EC2 Default User <ec2-user@ip-172-31-33-245.ap-northeast-1.compute.internal>
2023-09-07 03:32:50 +0000

    [finish]応用課題4

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,17 +8,17 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
-  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id"
-  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id"
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followings, through: :active_relationships, source: :followed
-  has_many :followers, through: :passive_relationships, source: :follewer
+  has_many :followers, through: :passive_relationships, source: :follower
 
   def follow(user)
     active_relationships.create(followed_id: user.id)
   end
 
   def unfollow(user)
-    active_relationships.create(followed_id: user.id)
+    active_relationships.find_by(followed_id: user.id).destroy
   end
 
   def following?(user)
